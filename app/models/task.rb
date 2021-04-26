2aa0aba1f898264f51a7063a5aabaf059860ab6a
UDAZ <u.ka2.sally.cinnamon@gmail.com>
2021-04-26 05:28:54 +0000

    add association

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,6 +2,8 @@ class Task < ApplicationRecord
     before_validation :set_nameless_name
     validates :name, presence: true, length: { maximum: 35}
     validate :validate_name_not_including_comma
+    
+    belongs_to :user
     private
     def set_nameless_name
         self.name = '名前なし' if name.blank?
