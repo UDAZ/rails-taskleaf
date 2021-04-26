376951773a85d1dbce2c53e25eb33cd0a1fb208a
UDAZ <u.ka2.sally.cinnamon@gmail.com>
2021-04-26 10:12:49 +0000

    fix ABCD

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -3,16 +3,16 @@ require 'rails_helper'
 describe 'タスク管理機能', type: :system do
     describe '一覧表示機能' do
         before do
-            # ユーザーAを作成しておく
-            # 作成者がユーザーAであるタスクを作成しておく
+            # ユーザーAを作成しておく-A
+            # 作成者がユーザーAであるタスクを作成しておく-B
         end
         context 'ユーザーAがログインしているとき' do
             before do
-            #ユーザーAでログインする
+            #ユーザーAでログインする -C
             end
             
             it 'ユーザーAが作成したタスクが表示される' do
-                # 作成済みのタスクの名称が画面上に表示されていることを確認
+                # 作成済みのタスクの名称が画面上に表示されていることを確認 -D
             end
         end
     end
